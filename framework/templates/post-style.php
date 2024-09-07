557e6a706f214c731d7a98ccde44d35cb239421f
Duc Dung <ducdung@192.168.102.40>
2024-09-07 11:21:59 +0700

    section Blog

--- framework/templates/post-style.php
+++ framework/templates/post-style.php
@@ -6,21 +6,23 @@ $category = get_the_terms($post_id, 'category');
   <div class="bt-post--inner">
     <?php echo chambeshi_post_cover_featured_render($args['image-size']); ?>
     <div class="bt-post--content">
-      <div class="bt-post--category">
-        <?php
-        if (!empty($category)) {
-          echo  '<a href="'.get_category_link($category[0]->term_id).'">'.$category[0]->name.'</a>';
-        }
-        ?>
-      </div>
+      <?php echo chambeshi_post_publish_render(); ?>
 
-      <?php echo chambeshi_post_title_render(); ?>
-    </div>
-    <div class="bt-post--info">
-        <?php
-        echo chambeshi_post_publish_render();
-        echo chambeshi_author_icon_render();
+      <div class="bt-post--infor">
+        <div class="bt-post--info">
+          <div class="bt-post--category">
+            <?php
+            if (!empty($category)) {
+              echo  '<a href="' . get_category_link($category[0]->term_id) . '">' . $category[0]->name . '</a>';
+            }
+            ?>
+          </div>
+          <?php echo chambeshi_reading_time_render(); ?>
+        </div>
+        <?php echo chambeshi_post_title_render();
+        echo chambeshi_author_w_avatar();
         ?>
       </div>
+    </div>
   </div>
 </article>
