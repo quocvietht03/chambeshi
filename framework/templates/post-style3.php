2ea69d37960945de8ac9502018be999b5fc5073b
Duc Dung <ducdung@Ducs-Mac-mini.local>
2024-10-18 10:24:50 +0700

    post loop item style 3

--- framework/templates/post-style3.php
+++ framework/templates/post-style3.php
@@ -9,25 +9,32 @@ $tags = get_the_terms($post_id, 'post_tag');
       <?php echo chambeshi_post_cover_featured_render($args['image-size']); ?>
       <div class="bt-post--tags">
         <?php
-         if (!empty($tags)) {
+        if (!empty($tags)) {
           echo  '<a href="' . get_tag_link($tags[0]->term_id) . '">' . $tags[0]->name . '</a>';
         }
         ?>
       </div>
+      <?php echo chambeshi_author_w_avatar_style2(); ?>
     </div>
     <div class="bt-post--content">
-      <?php echo chambeshi_author_w_avatar_style2();
+      <?php
       echo chambeshi_post_title_render();
-      echo chambeshi_post_publish_render_style2();
       ?>
+      <div class="bt-post--excerpt">
+        <?php echo get_the_excerpt(); ?>
+      </div>
       <div class="bt-post--line-ellipse">
         <svg xmlns="http://www.w3.org/2000/svg" width="438" height="2" viewBox="0 0 438 2" fill="none">
           <path d="M219 2C339.95 2 438 1.55228 438 1C438 0.447715 339.95 0 219 0C98.0496 0 0 0.447715 0 1C0 1.55228 98.0496 2 219 2Z" fill="#D5D5D5" />
         </svg>
       </div>
-      <?php
-      echo chambeshi_post_button_render('Read More');
-      ?>
+      <div class="bt-post--infor">
+        <?php
+        echo chambeshi_post_publish_render_style2();
+        echo chambeshi_post_button_render('Read More');
+        ?>
+      </div>
+
     </div>
 
   </div>
--- framework/widget-load.php
+++ framework/widget-load.php
@@ -60,6 +60,7 @@ class ElementorWidgets
 			'post-loop-item',
 			'post-loop-item-style-1',
 			'post-loop-item-style-2',
+			'post-loop-item-style-3',
 			'testimonial-slider',
 			'testimonial-loop-item',
 			'testimonial-loop-item-style-1',
@@ -188,6 +189,7 @@ class ElementorWidgets
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\PostLoopItem\Widget_PostLoopItem());
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\PostLoopItemStyle1\Widget_PostLoopItemStyle1());
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\PostLoopItemStyle2\Widget_PostLoopItemStyle2());
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\PostLoopItemStyle3\Widget_PostLoopItemStyle3());
 
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\TestimonialSlider\Widget_TestimonialSlider());
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Widgets\TestimonialLoopItem\Widget_TestimonialLoopItem());
