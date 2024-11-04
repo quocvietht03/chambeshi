7a00b1cf5f59fe1121661549a393f2629c023b14
Duc Dung <ducdung@Dung-Mac-mini.local>
2024-11-04 11:19:00 +0700

    page search

--- search.php
+++ search.php
@@ -7,7 +7,9 @@ get_template_part('framework/templates/site', 'titlebar');
 	<div class="bt-main-content-ss">
 		<div class="bt-container">
 			<div class="bt-form-search">
-			<?php get_search_form(); ?>
+				<h2 class="bt-form-head"><?php esc_html_e( 'Need a new search?', 'chambeshi' ) ?></h2>
+				<p class="bt-form-subhead"><?php esc_html_e( "If you didn't find what you were looking for, try a new search!", "chambeshi" ) ?></p>
+				<?php get_search_form(); ?>
 			</div>
 			<?php
 			if (have_posts()) {
@@ -15,7 +17,7 @@ get_template_part('framework/templates/site', 'titlebar');
 				<div class="bt-list-post-grid">
 					<?php
 					while (have_posts()) : the_post();
-					get_template_part( 'framework/templates/post', 'style', array('image-size' => 'large')); 
+						get_template_part('framework/templates/post', 'style', array('image-size' => 'large'));
 					endwhile;
 					?>
 				</div>
