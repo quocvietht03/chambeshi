16e854d1ba6cd691592e962c26873981bdf45d1e
tomdevvn90 <tomdevvn90@gmail.com>
2024-11-07 13:38:31 +0700

    update theme unit test

--- 404.php
+++ 404.php
@@ -15,10 +15,10 @@ get_template_part('framework/templates/site', 'titlebar');
 					<span><?php esc_html_e('Sorry! This Page Was Lost', 'chambeshi'); ?></span>
 					<h3><span><?php esc_html_e('Oppoos...', 'chambeshi'); ?></span><?php esc_html_e('Page Not Found!', 'chambeshi'); ?></h3>
 					<p><?php esc_html_e('The page you looking for not found may be it not exist or removed.', 'chambeshi'); ?></p>
-					<a class="bt-button" href="<?php echo home_url('/'); ?>"><?php esc_html_e('Back To Home', 'chambeshi'); ?></a>
+					<a class="bt-button" href="<?php echo esc_url(home_url('/')); ?>"><?php esc_html_e('Back To Home', 'chambeshi'); ?></a>
 				</div>
 				<div class="bt-404--image">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/404-min.jpg" alt="" />
+					<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/404-min.jpg'); ?>" alt="" />
 				</div>
 			</div>
 		</div>
